2a9091c0f042e028d2828602c56149893f12e399
Indexal <148807519+Index-al@users.noreply.github.com>
2023-11-20 16:47:44 -0600

    Added additional questions, added nextQuestion() function and Quiz Complete! screen.

--- develop/js/script.js
+++ develop/js/script.js
@@ -17,7 +17,26 @@ var questions = [
         answerChoices: ["strings", "booleans", "alerts", "numbers"],
         correctAnswer: "alerts"
     },
-    // TODO: Add more questions
+    {
+        questionText: "The condition in an if / else statement is enclosed within ____.",
+        answerChoices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: "parentheses"
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store ____.",
+        answerChoices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        questionText: "String values must be enclosed within ____ when being assigned to variables.",
+        answerChoices: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "quotes"
+    },
+    {
+        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answerChoices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        correctAnswer: "console.log"
+    }
 ];
 
 function displayQuestion() {
@@ -41,7 +60,21 @@ function displayQuestion() {
 
         // Add a click event listener to each choice item
         choiceListItem.addEventListener('click', function () {
-            // TODO: Handle user's choice selection
+            var selectedAnswer = choice; // The text content of the clicked choice
+
+            // Check if the answer is correct
+            if (selectedAnswer === currentQuestion.correctAnswer) {
+                if (currentQuestionIndex < questions.length) {
+                    timerCount = timerCount + 10;
+                }
+                console.log("Correct answer selected. Moving to the next question.");
+            } else {
+                timerCount = timerCount - 10;
+                console.log("Incorrect answer selected. Moving to the next question.");
+            }
+            
+            // Move onto the next question
+            nextQuestion();
         });
 
         answerChoicesList.appendChild(choiceListItem);
@@ -55,6 +88,21 @@ function displayQuestion() {
     quiz.appendChild(answerChoicesList);
 }
 
+function nextQuestion() {
+    // Increment the current question index
+    currentQuestionIndex++;
+
+    // Check if there are more questions
+    if (currentQuestionIndex < questions.length) {
+        // Display the next question
+        displayQuestion();
+    } else {
+        // Quiz is complete, do something (e.g., show score)
+        quiz.innerHTML = '<p>Quiz complete! Your score is: </p> <h3>' + timerCount + '</h3>';
+        timerEl.classList.add('hidden');
+        timerInterval = clearInterval(timerInterval);
+    }
+}
 
 // Event listeners for button clicks
 startBtn.addEventListener("click", startQuiz);
